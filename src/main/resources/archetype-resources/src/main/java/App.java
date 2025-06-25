de35c48f707c2365ce496a4e1c1c3d93285aa438
phquartin <phquartinmatos@sempreceub.com>
2025-06-25 01:31:50 -0300

    init: Primeiras Dependencias e Configurações do projeto.

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $br.com.agenciaviagens;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
